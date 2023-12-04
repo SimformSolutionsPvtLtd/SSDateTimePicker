7d300e29cbf99206e33ad1dce283f0fdaa13934e
Rizwana <rizwana@simformsolutions.com>
2023-12-04 11:43:39 +0530

    UNT-T20317: PR comments fixes, optimization and date picker configuration changes

--- DateTimePicker/DateTimePicker/Common/CornerRadiusStyle.swift
+++ DateTimePicker/DateTimePicker/Common/CornerRadiusStyle.swift
@@ -10,22 +10,32 @@ import SwiftUI
 
 struct CornerRadiusStyle: ViewModifier {
     
+    //MARK: - Property
+
     var radius: CGFloat
     var corners: UIRectCorner
+
+    //MARK: - Body
     
-    struct CornerRadiusShape: Shape {
-
-        var radius = CGFloat.infinity
-        var corners = UIRectCorner.allCorners
-
-        func path(in rect: CGRect) -> Path {
-            let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
-            return Path(path.cgPath)
-        }
-    }
-
     func body(content: Content) -> some View {
         content
             .clipShape(CornerRadiusShape(radius: radius, corners: corners))
     }
+    
+}
+
+struct CornerRadiusShape: Shape {
+    
+    //MARK: - Property
+    
+    var radius = CGFloat.infinity
+    var corners = UIRectCorner.allCorners
+    
+    //MARK: - Methods
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+    
 }
--- DateTimePicker/DateTimePicker/Extensions/View+Extension.swift
+++ DateTimePicker/DateTimePicker/Extensions/View+Extension.swift
@@ -27,4 +27,8 @@ extension View {
         ModifiedContent(content: self, modifier: CornerRadiusStyle(radius: radius, corners: corners))
     }
     
+    func themeButton(_ txtColor: Color) -> some View {
+        self.modifier(ThemeButtonModifier(textColor: txtColor))
+    }
+    
 }
